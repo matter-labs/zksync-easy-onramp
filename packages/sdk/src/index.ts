1d28e2db0c58b79993583f11f928657508e3087c
Sabrina Ferguson <sf@matterlabs.dev>
2025-02-13 18:09:15 -0500

    chore: update sdk exports

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -4,5 +4,12 @@ export {
   config,
   createConfig,
 } from "@sdk/config";
-export { executeRoute, } from "@sdk/core/execution";
-export type { RequestQuoteParams, Route, } from "@sdk/types/sdk";
+export { executeRoute, resumeExecution, } from "@sdk/core/execution";
+export type {
+  Execution, ExecutionStatus, Process, ProcessStatus,ProcessType, RequestQuoteParams, Route, SDKConfig,
+  Services,
+  StepExtended,
+} from "@sdk/types/sdk";
+export type {
+  ProviderQuoteOption, QuotesResponse, QuoteStep,
+} from "@sdk/types/server";
