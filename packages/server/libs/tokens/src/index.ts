efd7cd46b53afb7c0d720a7056644cd5b1d00950
Jack Hamer <jackhamer0990@gmail.com>
2025-02-18 21:38:54 +0200

    fix: tokens db symbol formatting

--- packages/server/libs/tokens/src/index.ts
+++ packages/server/libs/tokens/src/index.ts
@@ -1,3 +1,15 @@
+import type { Address, } from "viem";
+
+export type TokenData = {
+  address: Address;
+  chainId: number;
+  symbol: string;
+  name: string;
+  decimals: number;
+  usdPrice: number;
+  marketCap: number;
+  iconUrl?: string;
+};
 export * from "./tokens.module";
 export * from "./tokens.service";
 export * from "./tokens-data-saver.service";
