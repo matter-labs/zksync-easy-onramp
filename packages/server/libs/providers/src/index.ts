bcb258e8900e84e88cdf20769bbee36ebd2f1e29
Jack Hamer <47187316+JackHamer09@users.noreply.github.com>
2025-03-05 21:49:54 +0200

    feat: config endpoint with the list of tokens, chains, providers (#24)

--- packages/server/libs/providers/src/index.ts
+++ packages/server/libs/providers/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./provider.interface";
 export * from "./providers.module";
 export * from "./providers-quote.service";
+export * from "./providers-registry.service";
 export * from "./providers-update.service";
